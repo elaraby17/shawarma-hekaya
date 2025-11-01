e8fbbfe3adbecf16e61a1e2021205c21df5919e7
unknown <elaraby98123@gmail.com>
2025-11-01 16:08:17 +0200

    js

--- script/script.js
+++ script/script.js
@@ -9,25 +9,24 @@ if (btnMode) {
 //____________________________________________________________________________________________________________________________
 // products data
 const products = [
-    { id: 1, title: "شاورما لحمة بالخلطه المكسكية حار", category: "الساندوتشات", discription: "", image: "./assists/image/food/shawirma-spasy.jpg", price: 80 },
-    { id: 2, title: "شاورما لحمة فرنساوي", category: "الساندوتشات", discription: "", image: "./assists/image/food/shawirma-chekin.jpg", price: 75 },
-    { id: 3, title: "شاورما فراخ فرنساوي حار", category: "الساندوتشات", discription: "", image: "./assists/image/food/shawirma-spasy-chekin.jpg", price: 70 },
-    { id: 4, title: "شاورما فراخ فرنساوي", category: "الساندوتشات", discription: "", image: "./assists/image/food/shawirma-meet.jpg", price: 65 },
+    { id: 1, title: "شاورما لحمة بالخلطه المكسكية ", category: "الساندوتشات", discription: "", image: "./assists/image/food/shawirma-spasy.jpg", price: 80, tags: ["حار"] },
+    { id: 2, title: "شاورما لحمة فرنساوي", category: "الساندوتشات", discription: "", image: "./assists/image/food/shawirma-chekin.jpg", price: 75, tags: ["حار", "عادي"] },
+    { id: 3, title: "شاورما فراخ بالخلطة المكسيكة ", category: "الساندوتشات", discription: "", image: "./assists/image/food/shawirma-spasy-chekin.jpg", price: 70, tags: ["حار"] },
+    { id: 4, title: "شاورما فراخ فرنساوي", category: "الساندوتشات", discription: "", image: "./assists/image/food/shawirma-meet.jpg", price: 65, tags: ["حار", "عادي"] },
 
-    { id: 5, title: "ميني شاورما لحمة", category: "الميني ساندوتش", discription: "", image: "./assists/image/food/mini-meet.jpg", price: 35 },
-    { id: 6, title: "ميني شاورما فراخ", category: "الميني ساندوتش", discription: "", image: "./assists/image/food/mini-chekin.jpg", price: 28 },
+    { id: 5, title: "ميني شاورما لحمة", category: "الميني ساندوتش", discription: "", image: "./assists/image/food/mini-meet.jpg", price: 35, tags: ["حار", "عادي"] },
+    { id: 6, title: "ميني شاورما فراخ", category: "الميني ساندوتش", discription: "", image: "./assists/image/food/mini-chekin.jpg", price: 28, tags: ["حار", "عادي"] },
 
-    { id: 7, title: "وجبة عربي لحمة", category: "وجبات عربي", discription: "", image: "./assists/image/food/araby-meet.jpg", price: 100 },
-    { id: 8, title: "وجبة عربي فراخ", category: "وجبات عربي", discription: "", image: "./assists/image/food/araby-chekin.jpg", price: 90 },
+    { id: 7, title: "وجبة عربي لحمة", category: "وجبات عربي", discription: "", image: "./assists/image/food/araby-meet.jpg", price: 100, tags: ["حار", "عادي"] },
+    { id: 8, title: "وجبة عربي فراخ", category: "وجبات عربي", discription: "", image: "./assists/image/food/araby-chekin.jpg", price: 90, tags: ["حار", "عادي"] },
 
-    { id: 9, title: "فتة شاورما لحمة", category: "الفتة", discription: "", image: "./assists/image/food/fata-meet.jpg", price: 90 },
-    { id: 10, title: "فتة شاورما فراخ", category: "الفتة", discription: "", image: "./assists/image/food/fata-chekin.jpg", price: 80 },
-    { id: 11, title: "فتة شاورما ميكس", category: "الفتة", discription: "", image: "./assists/image/food/fata-mix.jpg", price: 90 },
+    { id: 9, title: "فتة شاورما لحمة", category: "الفتة", discription: "", image: "./assists/image/food/fata-meet.jpg", price: 90, tags: ["حار", "عادي"] },
+    { id: 10, title: "فتة شاورما فراخ", category: "الفتة", discription: "", image: "./assists/image/food/fata-chekin.jpg", price: 80, tags: ["حار", "عادي"] },
+    { id: 11, title: "فتة شاورما ميكس", category: "الفتة", discription: "", image: "./assists/image/food/fata-mix.jpg", price: 90, tags: ["حار", "عادي"] },
 
-    { id: 12, title: "بطاطس", category: "المقبلات", discription: "", image: "./assists/image/food/frise.png", price: 25 },
-    { id: 13, title: "كول سلو", category: "المقبلات", discription: "", image: "./assists/image/food/clowslow.png", price: 22 },
-    { id: 14, title: "توميه", category: "المقبلات", discription: "", image: "./assists/image/food/tomiah.jpg", price: 5 },
-
+    { id: 12, title: "بطاطس", category: "المقبلات", discription: "", image: "./assists/image/food/frise.png", price: 25, tags: "" },
+    { id: 13, title: "كول سلو", category: "المقبلات", discription: "", image: "./assists/image/food/clowslow.png", price: 22, tags: "" },
+    { id: 14, title: "توميه", category: "المقبلات", discription: "", image: "./assists/image/food/tomiah.jpg", price: 5, tags: "" },
 ];
 //____________________________________________________________________________________________________________________________
 let categoryType = "الكل";
@@ -53,17 +52,18 @@ function handelCategories() {
     let html = ``;
     results.forEach((result) => {
         html += `
-            <li class="category-list"><h3 data-value="${result.value}" class="category-item cursor-pointer px-4 py-2 rounded-lg hover:bg-amber-100 hover:text-amber-500 transition ${result.value === "الكل" ? "active bg-amber-100 text-amber-500" : ""}">${result.label}</h3></li>
+            <li class="category-list"><h3 data-value="${result.value}" class="category-item cursor-pointer px-4 py-2 rounded-lg hover:bg-amber-100 hover:text-amber-500 transition ${result.value === "الكل" ? "active bg-amber-100 text-amber-500" : ""
+            }">${result.label}</h3></li>
         `;
     });
     categoryContainer.innerHTML = html;
     // select the generated category items from the same container
-    const items = categoryContainer.querySelectorAll('h3');
+    const items = categoryContainer.querySelectorAll("h3");
     items.forEach((item) => {
         item.onclick = () => {
             categoryType = item.dataset.value;
-            items.forEach((it) => it.classList.remove('active', 'bg-amber-100', 'text-amber-500'));
-            item.classList.add('active', 'bg-amber-100', 'text-amber-500');
+            items.forEach((it) => it.classList.remove("active", "bg-amber-100", "text-amber-500"));
+            item.classList.add("active", "bg-amber-100", "text-amber-500");
             handelFilterProduct();
         };
     });
@@ -82,6 +82,7 @@ function handelDisplayProduct(data) {
                         <p class="text-gray-600 mb-4">${product.discription}</p>
                         <div class="flex items-center justify-between">
                             <span class="text-amber-600 font-bold text-lg">${product.price} ج.م</span>
+                           ${product.tags ? `<div class="tag-container "> ${product.tags.map((tag) => `<span class="tag bg-transparent border border-amber-600 text-amber-600 px-2 py-1 rounded-md mr-2">${tag}</span>`).join("")} </div>` : ``}
                         </div>
                     </div>
             </div>
